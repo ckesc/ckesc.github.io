d675b814e9c1e69be0d0a8feebb8ac26b8e4a502
CkEsc <iamckesc@gmail.com>
2014-06-23 10:14:49 +0400

    теоретическая поддержка touch начальной позиции

--- fh/eye_tracking.js
+++ fh/eye_tracking.js
@@ -1,14 +1,6 @@
-var mouse_captured = false;
+var startpos_captured = false;
 var start_x = 0;
 var start_y = 0;
-
-function moveEyes(x, y) {
-    moveEye (1, x, y, +100,  +600, +150, +150);
-    moveEye (2, x, y, -270,  -670, +210, +210);
-    moveEye (3, x, y, +270,  +270, +400, +50);
-    moveEye (4, x, y, +1000, +200, -300, -100);
-    moveEye (5, x, y, +700,  +500, +100, +500);
-}
 
 function moveEye(number, x, y, maxX, minX, maxY, minY) {
     var translateX;
@@ -31,11 +23,19 @@ function moveEye(number, x, y, maxX, minX, maxY, minY) {
     });
 }
 
+function moveEyes(x, y) {
+    moveEye (1, x, y, +100,  +600, +150, +150);
+    moveEye (2, x, y, -270,  -670, +210, +210);
+    moveEye (3, x, y, +270,  +270, +400, +50);
+    moveEye (4, x, y, +1000, +200, -300, -100);
+    moveEye (5, x, y, +700,  +500, +100, +500);
+}
+
 function captureFirstPos(x, y) {
-    if (!mouse_captured) {
+    if (!startpos_captured) {
         start_x = x;
         start_y = y;
-        mouse_captured = true;
+        startpos_captured = true;
         console.log('start pos '+x+' '+y);
     }
 }
@@ -45,7 +45,9 @@ function eyeTrack() {
     window.onmousemove = handleMouse;
 
     //touch
-    window.addEventListener("touchmove", handleTouch, false);
+    window.addEventListener("touchmove", handleTouchMove, false);
+    window.addEventListener("touchdown", handleTouchBegin, false);
+    window.addEventListener("touchup", handleTouchEnd, false);
 
     function handleMouse(event) {
         event = event || window.event; // IE-ism
@@ -74,7 +76,15 @@ function eyeTrack() {
         moveEyes(x,y);
     }
 
-    function handleTouch(event) {
+    function handleTouchBegin(event) {
+        captureFirstPos(touches[0].pageX, touches[0].pageY);
+    }
+        
+    function handleTouchEnd(event) {
+        startpos_captured = false;
+    }
+
+    function handleTouchMove(event) {
         event.preventDefault();
         var touches = event.changedTouches;
 
